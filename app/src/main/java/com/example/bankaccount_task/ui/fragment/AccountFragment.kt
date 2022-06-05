5d4a3a58681bb24a09648d759b4613e8d5d2a068
MohamedKamal16 <84516222+MohamedKamal16@users.noreply.github.com>
2022-06-05 13:23:38 +0200

    sparkFirstTask

---
+++ app/src/main/java/com/example/bankaccount_task/ui/fragment/AccountFragment.kt
@@ -0,0 +1,42 @@
+package com.example.bankaccount_task.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.bankaccount_task.databinding.FragmentAccountsBinding
+import com.example.bankaccount_task.ui.viewModel.AccountViewModel
+
+class AccountFragment : Fragment() {
+    private var _binding: FragmentAccountsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val slideshowViewModel =
+            ViewModelProvider(this).get(AccountViewModel::class.java)
+
+        _binding = FragmentAccountsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSlideshow
+        slideshowViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/bankaccount_task/ui/fragment/UserFragment.kt
@@ -0,0 +1,43 @@
+package com.example.bankaccount_task.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.bankaccount_task.databinding.FragmentUsersBinding
+import com.example.bankaccount_task.ui.viewModel.UserViewModel
+
+class UserFragment : Fragment() {
+
+    private var _binding: FragmentUsersBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val galleryViewModel =
+            ViewModelProvider(this).get(UserViewModel::class.java)
+
+        _binding = FragmentUsersBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textGallery
+        galleryViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
