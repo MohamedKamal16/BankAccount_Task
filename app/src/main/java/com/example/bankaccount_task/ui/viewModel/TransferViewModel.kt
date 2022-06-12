2b548ab76dd7be36b6fca77475ed48609efc6397
MohamedKamal16 <84516222+MohamedKamal16@users.noreply.github.com>
2022-06-12 15:05:20 +0200

    all Users /one user

--- app/src/main/java/com/example/bankaccount_task/ui/viewModel/TransferViewModel.kt
+++ app/src/main/java/com/example/bankaccount_task/ui/viewModel/TransferViewModel.kt
@@ -1,14 +1,13 @@
 package com.example.bankaccount_task.ui.viewModel
 
-
 import androidx.lifecycle.ViewModel
 import com.example.bankaccount_task.model.local.Repo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class UserViewModel@Inject constructor(private val repository: Repo) : ViewModel() {
-
-fun getUsers()=repository.getAllUsers()
+class TransferViewModel@Inject constructor(private val repository: Repo)  : ViewModel() {
+    fun getUsers()=repository.getAllUsers()
+  fun  getUserNameByAccountId(accountId:String)=repository.getUserNameByAccountId(accountId)
 
 }
